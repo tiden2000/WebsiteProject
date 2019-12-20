60b46e0701ccd563682035a391b8be46a9450085
tiden2000 <tiden2000@gmail.com>
2019-12-20 19:59:59 +0700

    change

--- database.php
+++ database.php
@@ -1,5 +1,5 @@
 <?php
-$serverName = "LAPTOP-7J3F2UBU\SQLEXPRESS, 1433"; //serverName\instanceName
+$serverName = "LAPTOP-7J3F2UBU\\SQLEXPRESS, 1433"; //serverName\instanceName
 
 // Since UID and PWD are not specified in the $connectionInfo array,
 // The connection will be attempted using Windows Authentication.
