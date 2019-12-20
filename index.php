ca9517361238092ef6c6e30a53a8ed6dbb642e96
tiden2000 <tiden2000@gmail.com>
2019-12-20 16:00:41 +0700

    Change

--- index.php
+++ index.php
@@ -15,28 +15,11 @@
                 </ul>
             </nav>
         </div>
-        <?php
-        $serverName = "LAPTOP-7J3F2UBU\\SQLEXPRESS"; //serverName\instanceName
-        
-        // Since UID and PWD are not specified in the $connectionInfo array,
-        // The connection will be attempted using Windows Authentication.
-        $connectionInfo = array( "Database"=>"ShopsDB");
-        $conn = sqlsrv_connect( $serverName, $connectionInfo);
-        $query = "SELECT Shop FROM Shops";
-        
-        if( $conn ) {
-            echo "Connection established.<br />";
-        }
-        else{
-            echo "Connection could not be established.<br />";
-            die( print_r( sqlsrv_errors(), true));
-        }
-        ?>
         <table>  <!-- table -->
         <tr>
-            <td>Shop</td>
+            <th>Shop</th>
         </tr>
-            <?php
+            <?php require 'database.php';
             while ($row = sqlsrv_fetch_array($query)) {
                 echo "<tr>";
                 echo "<td>".$row[Shop]."</td>";
