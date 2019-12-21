42d3115d8fcadb4462b19646dbe395ea3f94d44b
tiden2000 <tiden2000@gmail.com>
2019-12-21 13:10:24 +0700

    changes

--- database.php
+++ database.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "127.0.0.1";
-$username = "tiden2000";
-$password = "chienbot123";
+$servername = "localhost";
+$username = "root";
+$password = "";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
